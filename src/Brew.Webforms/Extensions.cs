cf6e3f2ea3a883f220fd2564c30430a3b621b101
shellscape <andrew@shellscape.org>
2013-03-07 22:43:30 -0500

    completed webforms control refactoring

---
+++ src/Brew.Webforms/Extensions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+using Brew.Webforms.Widgets;
+
+namespace Brew.Webforms {
+	public static class Extensions {
+
+		public static PropertyInfo GetProperty(this Type type, WidgetOption option) {
+			var list = type.GetProperties();
+			PropertyInfo result = null;
+
+			if (!String.IsNullOrEmpty(option.PropertyName)) {
+				result = list.Where(p => p.Name == option.PropertyName).FirstOrDefault();
+			}
+
+			if (result == null) {
+				result = list.Where(p => p.Name.ToLower() == option.Name.ToLower()).FirstOrDefault();
+			}
+
+			return result;
+		}
+
+		/// <summary>
+		/// Allows Linq operations on the controls collection.
+		/// </summary>
+		internal static IEnumerable<MenuItem> All(this List<MenuItem> items) {
+			foreach (MenuItem item in items) {
+				foreach (MenuItem grandChild in item.Items.All())
+					yield return grandChild;
+
+				yield return item;
+			}
+		}
+
+	}
+}
--- src/Brew.Webforms/Widgets/Accordion.cs
+++ src/Brew.Webforms/Widgets/Accordion.cs
@@ -10,8 +10,6 @@ namespace Brew.Webforms.Widgets {
 
 	[PersistChildren(true)]
 	[ParseChildren(typeof(AccordionPanel), DefaultProperty = "AccordionPanels", ChildrenAsProperties = true)]
-	[WidgetEvent("create")]
-	[WidgetEvent("beforeActivate")]
 	public class Accordion : BrewScriptControl, IAutoPostBackWidget {
 
 		private List<AccordionPanel> _panels;
@@ -20,141 +18,39 @@ namespace Brew.Webforms.Widgets {
 			this._panels = new List<AccordionPanel>();
 		}
 
-		#region Widget Options
+		public override List<WidgetEvent> GetEvents() {
+			return new List<WidgetEvent>() {
+				new WidgetEvent("create"),
+				new WidgetEvent("beforeActivate"),
+				new WidgetEvent("activate") { CausesPostBack = true, EventName = "ActivePanelChanged" }
+			};
+		}
 
-		/// <summary>
-		/// Selector for the active element. Set to false to display none at start. Needs collapsible: true.
-		/// Reference: http://api.jqueryui.com/accordion/#option-active
-		/// </summary>
-		[WidgetOption("active", "0")]
-		[TypeConverter(typeof(StringToObjectConverter))]
-		[Category("Layout")]
-		[DefaultValue("0")]
-		[Description("Selector for the active element. Set to false to display none at start. Needs collapsible: true.")]
-		public dynamic Active { get; set; }
-
-		/// <summary>
-		/// Choose your favorite animation, or disable them (set to false). In addition to the default, 'bounceslide' and all defined easing methods are supported ('bounceslide' requires UI Effects Core).
-		/// Reference: http://api.jqueryui.com/accordion/#option-animated
-		/// </summary>
-		[WidgetOption("animate", "{}", Eval = true)]
-		[TypeConverter(typeof(Brew.TypeConverters.JsonObjectConverter))]
-		[Category("Behavior")]
-		[DefaultValue("{}")]
-		[Description("Choose your favorite animation, or disable them (set to false). In addition to the default, 'bounceslide' and all defined easing methods are supported ('bounceslide' requires UI Effects Core).")]
-		public string Animate { get; set; }
-		
-		/// <summary>
-		/// Whether all the sections can be closed at once. Allows collapsing the active section by the triggering event (click is the default).
-		/// Reference: http://api.jqueryui.com/accordion/#option-collapsible
-		/// </summary>
-		[WidgetOption("collapsible", false)]
-		[Category("Behavior")]
-		[DefaultValue(false)]
-		[Description("Whether all the sections can be closed at once. Allows collapsing the active section by the triggering event (click is the default).")]
-		public bool Collapsible { get; set; }
-
-		/// <summary>
-		/// The event on which to trigger the accordion.
-		/// Reference: http://api.jqueryui.com/accordion/#option-event
-		/// </summary>
-		[WidgetOption("event", "click")]
-		[Category("Behavior")]
-		[DefaultValue("click")]
-		[Description("The event on which to trigger the accordion.")]
-		public string Event { get; set; }
-
-		/// <summary>
-		/// Selector for the header element.
-		/// Reference: http://api.jqueryui.com/accordion/#option-header
-		/// </summary>
-		[WidgetOption("header", "> li > :first-child,> :not(li):even")]
-		[Category("Layout")]
-		[DefaultValue("> li > :first-child,> :not(li):even")]
-		[Description("Selector for the header element.")]
-		public string Header { get; set; }
-
-		/// <summary>
-		/// Controls the height of the accordion and each panel. Possible values: 
-		/// "auto": All panels will be set to the height of the tallest panel. 
-		/// "fill": Expand to the available height based on the accordion's parent height. 
-		/// "content": Each panel will be only as tall as its content.
-		/// Reference: http://api.jqueryui.com/accordion/#option-heightstyle
-		/// </summary>
-		[WidgetOption("heightStyle", "{}", Eval = true)]
-		[TypeConverter(typeof(Brew.TypeConverters.JsonObjectConverter))]
-		[Category("Appearance")]
-		[DefaultValue("{}")]
-		[Description(@"Controls the height of the accordion and each panel. Possible values: 
-""auto"": All panels will be set to the height of the tallest panel. 
-""fill"": Expand to the available height based on the accordion's parent height. 
-""content"": Each panel will be only as tall as its content.")]
-		public string HeightStyle { get; set; }
-
-		/// <summary>
-		/// Icons to use for headers. Icons may be specified for 'header' and 'headerSelected', and we recommend using the icons native to the jQuery UI CSS Framework manipulated by jQuery UI ThemeRoller
-		/// Reference: http://api.jqueryui.com/accordion/#option-icons
-		/// </summary>
-		[WidgetOption("icons", "{}", Eval = true)]
-		[TypeConverter(typeof(Brew.TypeConverters.JsonObjectConverter))]
-		[Category("Appearance")]
-		[DefaultValue("{}")]
-		[Description("Icons to use for headers. Icons may be specified for 'header' and 'headerSelected', and we recommend using the icons native to the jQuery UI CSS Framework manipulated by jQuery UI ThemeRoller")]
-		public string Icons { get; set; }
-
-		/// <summary>
-		/// If set, looks for the anchor that matches location.href and activates it. Great for href-based state-saving. Use navigationFilter to implement your own matcher.
-		/// Reference: http://api.jqueryui.com/accordion/#option-navigation
-		/// </summary>
-		[Obsolete("This option will be removed in jQuery UI 1.10.")]
-		public bool Navigation { get; set; }
-
-		/// <summary>
-		/// Overwrite the default location.href-matching with your own matcher.
-		/// Reference: http://api.jqueryui.com/accordion/#option-navigationFilter
-		/// </summary>
-		[Obsolete("This option will be removed in jQuery UI 1.10.")]
-		public string NavigationFilter { get; set; }
-
-		#endregion
-
-		#region Obsolete Widget Options
-
-		[Obsolete("This property has been deprecated in jQuery UI 1.9. Use Animate instead.", true)]
-		public string Animated { get; set; }
-
-		[Obsolete("This property has been deprecated in jQuery UI 1.9. Use HeightStyle instead.", true)]
-		public bool AutoHeight { get; set; }
-
-		[Obsolete("This property has been deprecated in jQuery UI 1.9. Use HeightStyle instead.", true)]
-		public bool ClearStyle { get; set; }
-
-		[Obsolete("This property has been deprecated in jQuery UI 1.9. Use HeightStyle instead.", true)]
-		public bool FillSpace { get; set; }
-
-		#endregion
-
-		#region Widget Events
+		public override List<WidgetOption> GetOptions() {
+			return new List<WidgetOption>() {
+				new WidgetOption { Name = "active", DefaultValue = "0" },
+				new WidgetOption { Name = "animate", DefaultValue = "{}" },
+				new WidgetOption { Name = "collapsible", DefaultValue = false },
+				new WidgetOption { Name = "event", DefaultValue = "click" },
+				new WidgetOption { Name = "header", DefaultValue = "> li > :first-child,> :not(li):even" },
+				new WidgetOption { Name = "heightStyle", DefaultValue = "{}" },
+				new WidgetOption { Name = "icons", DefaultValue = "{}" }
+			};
+		}
 
 		/// <summary>
 		/// This event is triggered every time the accordion changes. If the accordion is animated, the event will be triggered upon completion of the animation; otherwise, it is triggered immediately.
 		/// 
 		/// Reference: http://api.jqueryui.com/accordion/#event-activate
 		/// </summary>
-		[WidgetEvent("activate", AutoPostBack = true)]
 		[Category("Action")]
 		[Description("This event is triggered when an accordion panel is shown.")]
 		public event EventHandler ActivePanelChanged;
 
-		[Obsolete("This event has been deprecated in jQuery UI 1.9. Use ActivePanelChanged instead.", true)]
-		public event EventHandler SelectedPanelChanged;
-
-		#endregion
-
 		[PersistenceMode(PersistenceMode.InnerProperty)]
 		[DesignerSerializationVisibility(DesignerSerializationVisibility.Content)]
-		public List<AccordionPanel> AccordionPanels { 
-			get { return this._panels;  }
+		public List<AccordionPanel> AccordionPanels {
+			get { return this._panels; }
 
 		}
 
@@ -171,6 +67,83 @@ namespace Brew.Webforms.Widgets {
 			}
 		}
 
+    #region .    Options    .
+
+		/// <summary>
+		/// Selector for the active element. Set to false to display none at start. Needs collapsible: true.
+		/// Reference: http://api.jqueryui.com/accordion/#option-active
+		/// </summary>
+		[TypeConverter(typeof(StringToObjectConverter))]
+		[Category("Layout")]
+		[DefaultValue("0")]
+		[Description("Selector for the active element. Set to false to display none at start. Needs collapsible: true.")]
+		public dynamic Active { get; set; }
+
+		/// <summary>
+		/// Choose your favorite animation, or disable them (set to false). In addition to the default, 'bounceslide' and all defined easing methods are supported ('bounceslide' requires UI Effects Core).
+		/// Reference: http://api.jqueryui.com/accordion/#option-animated
+		/// </summary>
+		[TypeConverter(typeof(Brew.TypeConverters.JsonObjectConverter))]
+		[Category("Behavior")]
+		[DefaultValue("{}")]
+		[Description("Choose your favorite animation, or disable them (set to false). In addition to the default, 'bounceslide' and all defined easing methods are supported ('bounceslide' requires UI Effects Core).")]
+		public string Animate { get; set; }
+		
+		/// <summary>
+		/// Whether all the sections can be closed at once. Allows collapsing the active section by the triggering event (click is the default).
+		/// Reference: http://api.jqueryui.com/accordion/#option-collapsible
+		/// </summary>
+		[Category("Behavior")]
+		[DefaultValue(false)]
+		[Description("Whether all the sections can be closed at once. Allows collapsing the active section by the triggering event (click is the default).")]
+		public bool Collapsible { get; set; }
+
+		/// <summary>
+		/// The event on which to trigger the accordion.
+		/// Reference: http://api.jqueryui.com/accordion/#option-event
+		/// </summary>
+		[Category("Behavior")]
+		[DefaultValue("click")]
+		[Description("The event on which to trigger the accordion.")]
+		public string Event { get; set; }
+
+		/// <summary>
+		/// Selector for the header element.
+		/// Reference: http://api.jqueryui.com/accordion/#option-header
+		/// </summary>
+		[Category("Layout")]
+		[DefaultValue("> li > :first-child,> :not(li):even")]
+		[Description("Selector for the header element.")]
+		public string Header { get; set; }
+
+		/// <summary>
+		/// Controls the height of the accordion and each panel. Possible values: 
+		/// "auto": All panels will be set to the height of the tallest panel. 
+		/// "fill": Expand to the available height based on the accordion's parent height. 
+		/// "content": Each panel will be only as tall as its content.
+		/// Reference: http://api.jqueryui.com/accordion/#option-heightstyle
+		/// </summary>
+		[TypeConverter(typeof(Brew.TypeConverters.JsonObjectConverter))]
+		[Category("Appearance")]
+		[DefaultValue("{}")]
+		[Description(@"Controls the height of the accordion and each panel. Possible values: 
+""auto"": All panels will be set to the height of the tallest panel. 
+""fill"": Expand to the available height based on the accordion's parent height. 
+""content"": Each panel will be only as tall as its content.")]
+		public string HeightStyle { get; set; }
+
+		/// <summary>
+		/// Icons to use for headers. Icons may be specified for 'header' and 'headerSelected', and we recommend using the icons native to the jQuery UI CSS Framework manipulated by jQuery UI ThemeRoller
+		/// Reference: http://api.jqueryui.com/accordion/#option-icons
+		/// </summary>
+		[TypeConverter(typeof(Brew.TypeConverters.JsonObjectConverter))]
+		[Category("Appearance")]
+		[DefaultValue("{}")]
+		[Description("Icons to use for headers. Icons may be specified for 'header' and 'headerSelected', and we recommend using the icons native to the jQuery UI CSS Framework manipulated by jQuery UI ThemeRoller")]
+		public string Icons { get; set; }
+
+		#endregion
+		
 		protected override void OnPreRender(EventArgs e) {
 
 			this.Controls.Clear();
